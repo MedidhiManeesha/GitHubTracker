1a6b8c014fcdaac7d14093432dea347e695fcfcd
Maneesha <medidhimaneesha@gmail.com>
2024-03-16 15:11:35 +0530

    Initial stage

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Tracker</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details.
+        Github Tracker is one of my React projects that I crafted to expand my skills.
+        
+      </p>
+      <p className='text-lg text-gray-400'>
+        My LinkedIn Profile
+      </p>
+      <p className='text-lg text-gray-400'>
+      <strong>
+          <a href='https://www.linkedin.com/in/maneesha-medidhi'>Maneesha Medidhi</a>
+        </strong>
+      </p>
+    </>
+  )
+}
+
+export default About
